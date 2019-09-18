30ba05e03a53a1d7b48dd15ee6b3327fc3af7422
VictorBayu <victorbayu85@gmail.com>
2019-09-18 15:18:09 +0700

    edit pada explicit dan implicit intent

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -9,10 +9,19 @@ import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    private EditText nama;
+    private TextView hasil;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        nama=findViewById(R.id.input_name);
+        hasil=findViewById(R.id.text_output);
     }
 
+    public void handleSubmit(View view) {
+        String name=nama.getText().toString();
+        hasil.setText(name);
+    }
 }
